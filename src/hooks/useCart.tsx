1f4e8dc9f0071592862da27c5dca07d8f9ff725e
rodrigodasilva <ribeiro.rodrigodasilva>
2021-04-20 18:31:07 -0300

    Adicionando itens ao carrinho

--- src/hooks/useCart.tsx
+++ src/hooks/useCart.tsx
@@ -19,24 +19,58 @@ interface CartContextData {
   updateProductAmount: ({ productId, amount }: UpdateProductAmount) => void;
 }
 
+const LOCAL_STORAGE_KEY = '@RocketShoes:cart';
+
 const CartContext = createContext<CartContextData>({} as CartContextData);
 
 export function CartProvider({ children }: CartProviderProps): JSX.Element {
   const [cart, setCart] = useState<Product[]>(() => {
-    // const storagedCart = Buscar dados do localStorage
+    const storagedCart = localStorage.getItem(LOCAL_STORAGE_KEY)
 
-    // if (storagedCart) {
-    //   return JSON.parse(storagedCart);
-    // }
+    if (storagedCart) {
+      return JSON.parse(storagedCart);
+    }
 
     return [];
   });
 
   const addProduct = async (productId: number) => {
     try {
-      // TODO
-    } catch {
-      // TODO
+      const cartProduct = cart.find(product => product.id === productId);
+      
+      const { data: stock } = await api.get<Product[]>('/stock');
+      const stockProduct = stock.find(product => product.id === productId)
+
+      if (!stockProduct) {
+        throw new Error('Erro na adição do produto')
+      }
+
+      let updatedCart: Product[] = [];
+
+      if (!cartProduct) {
+        updatedCart = [...cart, {...stockProduct, amount: 1 }];
+      } else {
+        updatedCart = cart.map(product => {
+          if (product.id !== productId) {
+            return product
+          }
+
+          const newAmount = product.amount + 1;
+          const hasStockAmount = stockProduct.amount >= newAmount;
+
+          if (!hasStockAmount) {
+            throw new Error('Quantidade solicitada fora de estoque')
+          }
+
+          return ({...product, amount: newAmount })       
+        });
+      }      
+
+      setCart(updatedCart)
+      localStorage.setItem(LOCAL_STORAGE_KEY, JSON.stringify(updatedCart))
+
+    } catch (err) {
+      toast.error(err.message)
     }
   };
 
